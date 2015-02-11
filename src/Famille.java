39817264ee81bae233d2b7dbb6bce028bab6595d
Fabien Pinel <pinel.fabien@gmail.com>
2015-02-11 19:11:46 +0100

    string enum et carte

--- src/Famille.java
+++ src/Famille.java
@@ -1,5 +1,17 @@
 
 public enum Famille {
-	HEART, SPADE, CLOVER, DIAMOND;
+	HEART("H"), SPADE("S"), CLOVER("C"), DIAMOND("D");
+
+	
+	private String str2;
+	
+	
+	private Famille(String str){
+		this.str2 = str;
+	}
+	
+	public String toString(){
+		return str2;
+	}
 	
 }
