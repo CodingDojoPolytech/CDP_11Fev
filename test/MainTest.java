6eaaac5a38064120ccadaaf3b4490fb93068f3ff
Fabien Pinel <pinel.fabien@gmail.com>
2015-02-11 19:41:56 +0100

    test decompose2 passe

--- test/MainTest.java
+++ test/MainTest.java
@@ -7,8 +7,13 @@ public class MainTest {
 
 	@Test
 	public void testDecompose() {
-		System.out.println((Main.decompose("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH"))[0].toString());
 		assertTrue((Main.decompose("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH"))[0].toString().equals("2H 3D 5S 9C KD"));
+	}
+	
+	@Test
+	public void testDecompose2() {
+		System.out.println((Main.decompose("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH"))[1].toString());
+		assertTrue((Main.decompose("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH"))[1].toString().equals("2C 3H 4S 8C AH"));
 	}
 
 }
